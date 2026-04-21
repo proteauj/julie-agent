3d276fe08764d6cfc00c11a94f24b4282a539a9c
Julie Proteau <julie.proteau@hotmail.com>
2026-04-21 12:50:22 -0400

    Fix build

--- backend/Memora.Api/Models/Reminder.cs
+++ backend/Memora.Api/Models/Reminder.cs
@@ -1,7 +1,9 @@
+using Memora.Api.Models;
 public class Reminder
 {
     public int Id { get; set; }
     public string UserId { get; set; } = "";
+    public User User { get; set; }
     public string Text { get; set; } = "";
     public DateTime Date { get; set; }
     public bool Done { get; set; } = false;
--- backend/Memora.Api/Services/RemindersBackgroundService.cs
+++ backend/Memora.Api/Services/RemindersBackgroundService.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Hosting;
+using Memora.Api.Services;
 
 public class RemindersBackgroundService : BackgroundService
 {
