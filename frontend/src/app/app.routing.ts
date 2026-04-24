656d5138f837bade204e2bc8124b0353029075ef
Julie Proteau <julie.proteau@hotmail.com>
2026-04-24 14:25:50 -0400

    Last a4.1 fix

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ProfileComponent } from './profile/profile.components';
 import { ChatComponent } from './chat/chat.component';
 import { AdminComponent } from './admin/admin.component';
@@ -11,17 +12,28 @@ import { RemindersComponent } from './reminders/reminders.component';
 import { ActivitiesComponent } from './activities/activities.component';
 
 const routes: Routes = [
-  { path: 'profil', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: 'admin', component: AdminComponent, canActivate: [AdminGuard] },
-  { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
-  { path: 'register', loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./register/register.component').then(m => m.RegisterComponent)
+  },
+
+  { path: 'home', component: SeniorHomeComponent, canActivate: [AuthGuard] },
+  { path: 'chat', component: ChatComponent, canActivate: [AuthGuard] },
   { path: 'agenda', component: AgendaComponent, canActivate: [AuthGuard] },
   { path: 'reminders', component: RemindersComponent, canActivate: [AuthGuard] },
   { path: 'activities', component: ActivitiesComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: 'chat' },
-  { path: 'home', component: SeniorHomeComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'chat', component: ChatComponent, canActivate: [AuthGuard] },
+  { path: 'profil', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [AdminGuard] },
+
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
