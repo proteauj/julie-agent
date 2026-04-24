cac64de82cde09ee9118696a65f223d3c3066ba1
Julie Proteau <julie.proteau@hotmail.com>
2026-04-24 15:53:53 -0400

    Try fix deploy front

--- frontend/src/app/app.translate.module.ts
+++ frontend/src/app/app.translate.module.ts
@@ -1,26 +1,41 @@
-import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-export function HttpLoaderFactory() {
+export function HttpLoaderFactory(): TranslateHttpLoader {
   return new TranslateHttpLoader();
+}
+
+export function initTranslate(translate: TranslateService) {
+  return () => {
+    translate.setDefaultLang('fr');
+    return translate.use('fr').toPromise();
+  };
 }
 
 @NgModule({
   exports: [
     TranslateModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   imports: [
     TranslateModule.forRoot({
-      defaultLanguage: 'fr',
       loader: {
         provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: []
+        useFactory: HttpLoaderFactory
       }
     })
+  ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initTranslate,
+      deps: [TranslateService],
+      multi: true
+    }
   ]
 })
 export class AppTranslateModule { }
